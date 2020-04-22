12c11a4e81af16621884c1071b214c9d1828849c
shujunge <zfwang@stu.xidian.edu.cn>
2020-04-23 00:16:30 +0800

    chore: Added many class inhert in manim.

--- manim_demo/run.py
+++ manim_demo/run.py
@@ -11,13 +11,14 @@ from srcs.CodeShow import Codeshow
 from srcs.Code_line import TestDecimalNumberText
 from srcs.ThreeD_demo import basic_3d_shape, update_text_timeing
 from srcs.TwoD_demo import my_numberplane
+from srcs.move_camera import change_camera_size
 
 
 
 class PhysicExample(PhysicScene):
     def construct(self):
-        self.set_gravity(9.8 * DOWN)
 
+        self.set_gravity(9.8 * DOWN)
         grd_body = self.space.static_body
         grd_seg = pymunk.Segment(grd_body, (-10000, -2000), (10000, -2000), 0)
         grd_seg.friction = 0.7
@@ -29,7 +30,7 @@ class PhysicExample(PhysicScene):
         ground = PhysicMobject(grd_body, grd_seg, grd_shape)
         self.add_static_obj(ground)
 
-        for i in range(30):
+        for i in range(60):
             mass = 10
             size = (500, 500)
             moment = pymunk.moment_for_box(mass, size)
@@ -39,9 +40,9 @@ class PhysicExample(PhysicScene):
             box = pymunk.Poly.create_box(body, size)
             box.friction = 0.5
 
-            shape = Square()
-            shape.stretch_to_fit_height(0.5)
-            shape.stretch_to_fit_width(0.5)
+            shape = Circle(radius=0.5)
+            # shape.stretch_to_fit_height(0.5)
+            # shape.stretch_to_fit_width(0.5)
             shape.set_fill(DARK_BLUE, opacity=1)
 
             mobj = PhysicMobject(body, box, shape)
@@ -51,6 +52,5 @@ class PhysicExample(PhysicScene):
 
         self.simulate(20)
 
-
 if __name__ == '__main__':
-    run(your_module = [Text_demo], l=True, gif =False)
+    run(your_module = [change_camera_size], l=True, gif =False)
--- manim_demo/srcs/move_camera.py
+++ manim_demo/srcs/move_camera.py
@@ -1,35 +1,34 @@
 from manimlib.imports import *
 
-class change_camera_size(MovingCameraScene):
+class Chapter1_Animation(MovingCameraScene):
 
     def construct(self):
-
-        ## 物体构建
-        text_1 = Text('摄像机位置缩放效果展示', font=' DFLianLian SC2 W2', color=BLUE).to_edge(TOP, buff=0.1)
-        text_2 = Text('让我们来回顾一个经典的问题', font='丁永康硬笔楷书新版', color=YELLOW).scale(0.3)
-        zz = Circle(radius=0.5)
-        obj = VGroup(zz, text_2).arrange_submobjects(DOWN, buff=0.5)
-
-        achilles_svg = SVGMobject('./srcs/figs/Achilles.svg', color=BLUE, stroke_width=0.2).scale(1.25).to_corner(
+        self.text_1 = Text('摄像机位置缩放效果展示', font=' DFLianLian SC2 W2', color='RED').to_edge(TOP, buff=0.1)
+        self.text_2 = Text('让我们来回顾一个经典的问题', font='丁永康硬笔楷书新版', color='YELLOW').scale(0.3)
+        self.achilles_svg = SVGMobject('./srcs/figs/Achilles.svg', color='BLUE', stroke_width=0.2).scale(
+            1.25).to_corner(
             DOWN * 1.8 + LEFT * 6)
-        tortoise_svg = SVGMobject('./srcs/figs/tortoise.svg', color=YELLOW).scale(0.64).to_corner(
+        self.tortoise_svg = SVGMobject('./srcs/figs/tortoise.svg', color='YELLOW').scale(0.64).to_corner(
             DOWN * 1.8 + RIGHT * 6)
-
+        self.circle_1 = Circle(radius=0.5)
+        self.obj = VGroup(self.circle_1, self.text_2).arrange_submobjects(DOWN, buff=0.5)
+    
+    def Chapter1_Animation(self):
         # 动画展示
-        self.play(Write(text_1), run_time=1.8)
+        self.play(Write(self.text_1), run_time=1.8)
         self.wait(0.6)
-        self.play(LaggedStartMap(FadeInFromDown, obj), run_time=2)
+        self.play(LaggedStartMap(FadeInFromDown, self.obj), run_time=2)
         self.wait(0.4)
-        self.play(FadeInFromLarge(achilles_svg))
-        self.play(FadeInFromLarge(tortoise_svg))
-        self.play(TurnInsideOut(achilles_svg), WiggleOutThenIn(tortoise_svg))
+        self.play(FadeInFromLarge(self.achilles_svg))
+        self.play(FadeInFromLarge(self.tortoise_svg))
+        self.play(TurnInsideOut(self.achilles_svg), WiggleOutThenIn(self.tortoise_svg))
         self.wait(2)
         self.camera.frame.save_state()
         self.play(
             # Set the size with the width of a object
-            self.camera.frame.set_width, text_2.get_width()*1.2,
+            self.camera.frame.set_width, self.text_2.get_width() * 1.2,
             # Move the camera to the object
-            self.camera.frame.move_to, text_2
+            self.camera.frame.move_to, self.text_2
         )
         self.wait()
         # Restore the state saved
@@ -37,10 +36,10 @@ class change_camera_size(MovingCameraScene):
 
         self.play(
             # Set the size with the width of a object
-            self.camera.frame.set_height, zz.get_height() * 1.5,
+            self.camera.frame.set_height, self.circle_1.get_height() * 1.5,
 
             # Move the camera to the object
-            self.camera.frame.move_to, zz
+            self.camera.frame.move_to, self.circle_1
         )
         self.wait()
         # Restore the state saved
@@ -48,9 +47,9 @@ class change_camera_size(MovingCameraScene):
 
         self.play(
             # Set the size with the width of a object
-            self.camera.frame.set_height, achilles_svg.get_height() * 1.2,
+            self.camera.frame.set_height, self.achilles_svg.get_height() * 1.2,
             # Move the camera to the object
-            self.camera.frame.move_to, achilles_svg
+            self.camera.frame.move_to, self.achilles_svg
         )
         self.wait()
 
@@ -59,13 +58,31 @@ class change_camera_size(MovingCameraScene):
 
         self.play(
             # Set the size with the width of a object
-            self.camera.frame.set_width, tortoise_svg.get_width() * 1.2,
+            self.camera.frame.set_width, self.tortoise_svg.get_width() * 1.2,
             # Move the camera to the object
-            self.camera.frame.move_to, tortoise_svg
+            self.camera.frame.move_to, self.tortoise_svg
         )
         self.wait()
         self.play(Restore(self.camera.frame))
 
-
-
         self.wait()
+
+class Chapter2_Animation(Chapter1_Animation):
+
+    def construct(self):
+        super(Chapter2_Animation, self).construct()
+
+        self.Chapter2_text_1 = Text('文字变换展示', font=' DFLianLian SC2 W2', color='GREEN').scale(2)
+        
+    def Chapter2_Animation(self):
+        # 动画展示
+        self.clear()
+        self.play(WiggleOutThenIn(self.Chapter2_text_1), run_time=1.8)
+        self.wait(0.6)
+
+class change_camera_size(Chapter2_Animation):
+
+    def construct(self):
+        super(change_camera_size, self).construct()
+        self.Chapter1_Animation()
+        self.Chapter2_Animation()
